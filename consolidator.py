5257232bb3f85077bad16f33b9a72e2f6060637a
Billy D <wild188@gmail.com>
2016-11-29 21:09:37 -0500

    array needs structs to be reshaped

--- consolidator.py
+++ consolidator.py
@@ -7,39 +7,40 @@ def readData():
 
     userInfo = np.load("datasets/userLog.npy")
     userInfo = np.delete(userInfo, (0), axis=0)
-    #print userInfo[:,0]
-    #purchaseInfo = np.load("datasets/purchase_info.npy")
-    #purchaseInfo = np.delete(purchaseInfo, (0), axis=0)
+    print("Read in user information")
+    purchaseInfo = np.load("datasets/purchase_info.npy")
+    purchaseInfo = np.delete(purchaseInfo, (0), axis=0)
+    print("Read in purchase information")
 
 def updateUserInfo(transaction):
     global userInfo
     global conUserInfo
-
-    index = np.sortedsearch(userInfo[:,0], transaction[0])
+    print conUserInfo
+    index = np.searchsorted(userInfo[:,0], transaction[0])
     #adding item_id to list
     try:
-        conUserInfo[index][3].append(transaction[1])
+        conUserInfo[index][3] = np.append(conUserInfo[index][3], transaction[1])
     except IndexError:
-        conUserInfo[index].append([transaction[1]])
+        conUserInfo[index] = np.append(conUserInfo[index],[transaction[1]])
     
     #adding category_id to list
     try:
-        conUserInfo[index][3].append(transaction[2])
+        conUserInfo[index][4] = np.append(conUserInfo[index][2],transaction[2])
     except IndexError:
-        conUserInfo[index].append([transaction[2]])
+        conUserInfo[index] = np.append(conUserInfo[index], [transaction[2]])
 
     #add merchant_id to list
     try:
-        conUserInfo[index][3].append(transaction[3])
+        conUserInfo[index][5] = np.append(conUserInfo[index][3], transaction[3])
     except IndexError:
         conUserInfo[index].append([transaction[3]])
 
     #add action to list
     try:
-        conUserInfo[index][4][transaction[4]] += 1
+        conUserInfo[index][6][transaction[4]] += 1
     except IndexError:
-        conUserInfo[index].append([0, 0, 0, 0])
-        conUserInfo[index][4][transaction[4]] += 1
+        conUserInfo[index] = np.append(conUserInfo[index], [0, 0, 0, 0])
+        conUserInfo[index][6][transaction[4]] += 1
 
 def updateMerchantInfo(transaction):
     global conMerchantInfo
@@ -52,11 +53,9 @@ def interpretTransaction(transaction):
 def consolidate():
     global userInfo
     global purchaseInfo
+    global consUserInfo
 
-    userInfo = np.array(userInfo)
-    userInfo.sort(axis=0)
-    conUserInfo = userInfo
-    print userInfo
+    
     for row in purchaseInfo:
         interpretTransaction(row)
 
@@ -74,5 +73,11 @@ purchaseInfo = np.array([])
 conUserInfo = np.array([])
 conMerchantInfo = np.array([])
 readData()
+
+userInfo = np.array(userInfo)
+userInfo.sort(axis=0)
+conUserInfo = np.copy(userInfo)
+print conUserInfo
+
 consolidate()
 saveResults()
